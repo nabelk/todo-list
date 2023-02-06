d17f4f48fbcede31ec3be3bc2e9b5e4e8ce1a65a
Nabil Khalid <holanabilkhalid@gmail.com>
2023-02-07 01:39:17 +0800

    Polishing based on other modules

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import _ from 'lodash';
+import HomeTab from './homeui';
 import Screen from './ui';
+import NotesTab from './notesui';
 
-const ui = Screen();
-
-ui.project.Addproject('Groceries');
-ui.project.addTodo(0, 'Beli Nasi', 'Nasi Goreng', '2023-01-06', 'high', false);
-ui.project.addTodo(
-    0,
-    'Beli Buah',
-    'Buah orange',
-    '2023-01-06',
-    'medium',
-    false
-);
-ui.project.addTodo(0, 'Beli Buah', 'Buah orange', '2023-01-06', 'medium', true);
-ui.renderProjectUI();
-document.querySelector(`[data-project-index="0"]`).click();
+NotesTab();
+Screen();
+HomeTab();
