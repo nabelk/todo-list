402115377bb29d2862af0d8e70be72e78145fb5d
Nabil Khalid <holanabilkhalid@gmail.com>
2023-02-08 02:50:40 +0800

    Modify for it to work with all modules

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,21 +1,21 @@
 export default function Storage() {
-    const checkLocalStorage = (arr) => {
-        if (localStorage.getItem('arr')) {
+    const checkLocalStorage = (arr, storage) => {
+        if (localStorage.getItem(storage)) {
             console.log('theres a storage');
-            JSON.parse(localStorage.getItem('arr')).forEach((item) => {
+            JSON.parse(localStorage.getItem(storage)).forEach((item) => {
                 arr.push(item);
             });
-            console.table(localStorage.getItem('arr'));
+            console.table(localStorage.getItem(storage));
         } else {
             console.log('no local storage');
-            console.log(localStorage.getItem('arr'));
+            console.log(localStorage.getItem(storage));
         }
     };
 
-    const updateLocalStorage = (arr) => {
-        localStorage.setItem('arr', JSON.stringify(arr));
+    const updateLocalStorage = (arr, storage) => {
+        localStorage.setItem(storage, JSON.stringify(arr));
         if (arr.length === 0) {
-            localStorage.removeItem('arr');
+            localStorage.removeItem(storage);
         }
     };
 
